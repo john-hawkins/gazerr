eb9b066d863862adfc2fdf1c1f4aadc10658f30e
John Hawkins <johnhawkins@Johns-MBP.modem>
2022-04-20 18:08:23 +1000

    Adding timer around runner for profiling

--- gazerr-runner.py
+++ gazerr-runner.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import time
  
 """Convenience wrapper for running gazerr directly from source tree."""
  
 from gazerr.cli import main
  
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print("---Execution:  %s seconds ---" % (time.time() - start_time))
 
